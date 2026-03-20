cfc6ee829d1858ecb74a09445d1a1d29ce91f4e6
Red-Lattice <err.error.found@gmail.com>
2026-03-20 13:39:50 -0400

    stride figured out

--- main.c
+++ main.c
@@ -6,5 +6,8 @@
 #include "stb_image_write.h"
 
 int main(int argc, char* argv[]) {
-    int ok = stbi_write_png("Fractran Spactime Diagram.png", );
+	int width = 0;
+	int height = 0;
+	int* data = NULL;
+    int ok = stbi_write_png("Fractran Spactime Diagram.png", width, height, 4, data, sizeof(*data) * width);
 }
